c5ef3a0f779452ca4ba1fe17051d373e329b79b8
tombarmusic <tombarmusic@gmail.com>
2023-05-27 00:20:56 +0300

    small fix

--- modules/paths.py
+++ modules/paths.py
@@ -9,4 +9,4 @@ FRAMES_DIR_PATH = OUTPUT_DIR_PATH + '/frames'
 # soundfonts
 SOUNDFONTS_DIR_PATH = ROOT_PATH + '/soundfonts'
 YAMAHA_C7_SF2_PATH = SOUNDFONTS_DIR_PATH + '/Yamaha_C7__Normalized_.sf2'
-CTK_533_SF2_PATH = SOUNDFONTS_DIR_PATH + 'CTK-533_Piano1__00___Casio_.sf2'
+CTK_533_SF2_PATH = SOUNDFONTS_DIR_PATH + '/CTK-533_Piano1__00___Casio_.sf2'
