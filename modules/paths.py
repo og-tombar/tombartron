cd5f2fce8aa960c435077cabd00367728a66f243
tombarmusic <tombarmusic@gmail.com>
2023-05-26 23:19:03 +0300

    remove frames

--- modules/paths.py
+++ modules/paths.py
@@ -5,3 +5,7 @@ MODULES_PATH = os.path.dirname(os.path.realpath(__file__))
 ROOT_PATH = os.path.dirname(MODULES_PATH)
 OUTPUT_DIR_PATH = ROOT_PATH + '/output'
 FRAMES_DIR_PATH = OUTPUT_DIR_PATH + '/frames'
+
+# soundfonts
+SOUNDFONTS_DIR_PATH = ROOT_PATH + '/soundfonts'
+YAMAHA_C7_SF2_PATH = SOUNDFONTS_DIR_PATH + '/Yamaha_C7__Normalized_.sf2'
--- tests.py
+++ tests.py
@@ -1,5 +1,24 @@
 from modules.all_utils import *
-# from modules.export_utils import *
-# from modules.paths import *
 
-empty_dir(FRAMES_DIR_PATH)
+import time
+import fluidsynth
+
+fs = fluidsynth.Synth()
+fs.start()
+
+sfid = fs.sfload(YAMAHA_C7_SF2_PATH)
+fs.program_select(0, sfid, 0, 0)
+
+fs.noteon(0, 60, 30)
+fs.noteon(0, 67, 30)
+fs.noteon(0, 76, 30)
+
+time.sleep(1.0)
+
+fs.noteoff(0, 60)
+fs.noteoff(0, 67)
+fs.noteoff(0, 76)
+
+time.sleep(1.0)
+
+fs.delete()
