d0976e1e2cc6abbd0d7555942ee7f0ab95f34b32
tombarmusic <tombarmusic@gmail.com>
2023-05-27 11:45:36 +0300

    audio codec fix

--- modules/video_utils.py
+++ modules/video_utils.py
@@ -7,11 +7,4 @@ def combine_frames_with_audio(frames_folder, audio_file, output_path):
     video_clip = ImageSequenceClip(frames, fps=60)
     audio_clip = AudioFileClip(audio_file)
     final_clip = video_clip.set_audio(audio_clip)
-    final_clip.write_videofile(output_path, codec='libx264')
-
-# # Example usage
-# frames_folder = FRAMES_DIR_PATH
-# audio_file = OUTPUT_DIR_PATH + '/simple_chord.wav'
-# output_path = OUTPUT_DIR_PATH + '/video.mp4'
-
-# combine_frames_with_audio(frames_folder, audio_file, output_path)
+    final_clip.write_videofile(output_path, codec='libx264', audio_codec='aac')
--- tests.py
+++ tests.py
@@ -1,19 +1,19 @@
-from modules.all_utils import *
+# from modules.all_utils import *
 
-import os
+# import os
 
-from moviepy.editor import ImageSequenceClip, AudioFileClip
+# from moviepy.editor import ImageSequenceClip, AudioFileClip
 
-def combine_frames_with_audio(frames_folder, audio_file, output_path):
-    frames = [f"{frames_folder}/{frame}" for frame in sorted(os.listdir(frames_folder))]
-    video_clip = ImageSequenceClip(frames, fps=60)
-    audio_clip = AudioFileClip(audio_file)
-    final_clip = video_clip.set_audio(audio_clip)
-    final_clip.write_videofile(output_path, codec='libx264')
+# def combine_frames_with_audio(frames_folder, audio_file, output_path):
+#     frames = [f"{frames_folder}/{frame}" for frame in sorted(os.listdir(frames_folder))]
+#     video_clip = ImageSequenceClip(frames, fps=60)
+#     audio_clip = AudioFileClip(audio_file)
+#     final_clip = video_clip.set_audio(audio_clip)
+#     final_clip.write_videofile(output_path, codec='libx264')
 
-# Example usage
-frames_folder = FRAMES_DIR_PATH
-audio_file = OUTPUT_DIR_PATH + '/simple_chord.wav'
-output_path = OUTPUT_DIR_PATH + '/video.mp4'
+# # Example usage
+# frames_folder = FRAMES_DIR_PATH
+# audio_file = OUTPUT_DIR_PATH + '/simple_chord.wav'
+# output_path = OUTPUT_DIR_PATH + '/video.mp4'
 
-combine_frames_with_audio(frames_folder, audio_file, output_path)
+# combine_frames_with_audio(frames_folder, audio_file, output_path)
