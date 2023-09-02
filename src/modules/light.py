98964d35ca920e6b220e7b89e425eafa767748d6
tombarmusic <tombarmusic@gmail.com>
2023-09-02 22:14:55 +0300

    small change

--- src/modules/light.py
+++ src/modules/light.py
@@ -11,14 +11,14 @@ class Light:
         glLightfv(GL_LIGHT0, GL_SPECULAR, (1, 1, 1, 1))
         glEnable(GL_DEPTH_TEST)
 
-    def activate(self):
+    def activate(self) -> None:
         glLight(GL_LIGHT0, GL_POSITION, self.position)
         glEnable(GL_LIGHTING)
         glEnable(GL_LIGHT0)
         glEnable(GL_COLOR_MATERIAL)
         glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
 
-    def deactivate(self):
+    def deactivate(self) -> None:
         glDisable(GL_LIGHT0)
         glDisable(GL_LIGHTING)
         glDisable(GL_COLOR_MATERIAL)
